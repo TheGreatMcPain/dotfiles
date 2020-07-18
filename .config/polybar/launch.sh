c3fdb7faa205ed7e44cf6152736c7b3cb6d1abe6
TheGreatMcPain <sixsupersonic@gmail.com>
2020-07-18 01:54:44 -0500

    polybar: Make temperature selectable via argument. Ex (select tempzone2): ./launch.sh 2

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,8 +13,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Start polybar on primary monitor first.
 # This makes sure the systray is on the correct monitor.
 PRIMARY=$(polybar --list-monitors | grep primary | cut -d":" -f1)
-MONITOR=$PRIMARY TRAYPOS=center polybar --reload default-top & echo $! > $pidFile-$PRIMARY.pid
-MONITOR=$PRIMARY TRAYPOS=center polybar --reload default-bottom & echo $! >> $pidFile-$PRIMARY.pid
+MONITOR=$PRIMARY TRAYPOS=center TEMPZONE=$1 polybar --reload default-top & echo $! > $pidFile-$PRIMARY.pid
+MONITOR=$PRIMARY TRAYPOS=center TEMPZONE=$1 polybar --reload default-bottom & echo $! >> $pidFile-$PRIMARY.pid
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     # Don't try to start a new bar on primary monitor.
