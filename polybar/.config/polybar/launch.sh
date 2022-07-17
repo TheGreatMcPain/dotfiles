c426206e7cf5bfe2dbf7638e3ab258cdeb82ee12
TheGreatMcPain <james@thegreatmcpain.xyz>
2022-07-17 02:02:45 -0500

    polybar: Use hwmon-path for temp readout

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -17,14 +17,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Start polybar on primary monitor first.
 # This makes sure the systray is on the correct monitor.
 PRIMARY=$(polybar --list-monitors | grep primary | cut -d":" -f1)
-MONITOR=$PRIMARY TRAYPOS=center TEMPZONE=$1 polybar --reload default-top & echo $! > $pidFile-$PRIMARY.pid
-MONITOR=$PRIMARY TRAYPOS=center TEMPZONE=$1 polybar --reload default-bottom & echo $! >> $pidFile-$PRIMARY.pid
+MONITOR=$PRIMARY TRAYPOS=center HWMONPATH="$1" polybar --reload default-top & echo $! > $pidFile-$PRIMARY.pid
+MONITOR=$PRIMARY TRAYPOS=center HWMONPATH="$1" polybar --reload default-bottom & echo $! >> $pidFile-$PRIMARY.pid
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     # Don't try to start a new bar on primary monitor.
     if ! [ $m = $PRIMARY ]; then
-	MONITOR=$m TEMPZONE=$1 polybar --reload default-top & echo $! > $pidFile-$m.pid
-        MONITOR=$m TEMPZONE=$1 polybar --reload default-bottom & echo $! >> $pidFile-$m.pid
+	MONITOR=$m HWMONPATH="$1" polybar --reload default-top & echo $! > $pidFile-$m.pid
+        MONITOR=$m HWMONPATH="$1" polybar --reload default-bottom & echo $! >> $pidFile-$m.pid
     fi
 done
 
