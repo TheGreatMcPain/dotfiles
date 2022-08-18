7fefb22d31a08782392d348dbc0a4751f6a84bd8
TheGreatMcPain <james@thegreatmcpain.xyz>
2022-08-18 00:50:15 -0500

    hyprland: Update startup script for nvidia
    As recommended by Hyprland's wiki for Nvidia users.

--- hyprland/.local/bin/start-hyprland.sh
+++ hyprland/.local/bin/start-hyprland.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0"
+export LIBVA_DRIVER_NAME=nvdec
+export CLUTTER_BACKEND=wayland
+export XDG_SESSION_TYPE=wayland
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export MOZ_ENABLE_WAYLAND=1
+export GBM_BACKEND=nvidia-drm
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_NO_HARDWARE_CURSORS=1
+export WLR_BACKEND=vulkan
+export QT_QPA_PLATFORM=wayland
+export GDK_BACKEND=wayland
 export XDG_CURRENT_DESKTOP="Unity"
 exec dbus-run-session Hyprland
